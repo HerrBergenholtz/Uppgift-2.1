439e51a6ecf06efe9d69b40769ba0669ff37c60f
HerrBergenholtz <zbdv8800@gmail.com>
2023-09-15 12:32:11 +0200

    Add project files.

---
+++ Uppgift 2.1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Uppgift_2._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Vad är din favoritbok?");
+            string bok = Console.ReadLine();
+            Console.WriteLine("Du gillar " + bok);
+
+            Console.ReadKey();
+        }
+    }
+}
